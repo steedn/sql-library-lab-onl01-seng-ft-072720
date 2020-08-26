ca6ee6a14ac8471c325e64dad6d82e638440773f
Nathan Steed <steedn@outlook.com>
2020-08-26 20:19:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -55,7 +55,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  <<-SQL
+  # <<-SQL
     SELECT characters.name, COUNT(*) as book_count
     FROM character_books
     JOIN characters
