ea94c77f9302fccd2ef8b287d1e636e80778c12b
Nathan Steed <steedn@outlook.com>
2020-08-26 20:06:01 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE series (
   author_id INTEGER,
   subgenre_id INTEGER
 );
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREAT TABLE authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
