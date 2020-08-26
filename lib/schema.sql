5afb9710a5b051c28295254167eadde0f376dc16
Nathan Steed <steedn@outlook.com>
2020-08-26 20:08:35 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -10,7 +10,22 @@ CREATE TABLE subgenres (
   name TEXT
 );
 
-CREAT TABLE authors(
+CREATE TABLE authors(
   id INTEGER PRIMARY KEY,
   name TEXT
 );
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY, 
+  year INTEGER, 
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT, 
+  species TEXT, 
+  motto TEXT,
+  author_id INTEGER
+);
+
