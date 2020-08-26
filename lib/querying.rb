2a40bdb5d951467c4f4bfcb08111abd2f29f27d1
Nathan Steed <steedn@outlook.com>
2020-08-26 20:22:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_name_and_series_subgenres_of_authors
     ON series.subgenre_id = subgenres.id"
 end
 
-def select_series_title_with_most_human_characters 
+def select_series_title_with_most_human_characters
     "SELECT series.title
     FROM series
     JOIN books
